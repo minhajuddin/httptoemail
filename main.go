5881e364a944919cde0080a9b4a75d76aacc9b19
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2013-02-08 02:36:43 +0530

    Added ability to send email to a fixed address

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/smtp"
+	"net/url"
 )
 
+var STD_RESPONSE = []byte("DONE")
+
 func main() {
-	log.Println("started")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got a request")
-		w.Write([]byte("This is cool"))
-	})
+	http.HandleFunc("/", requestHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func sendEmail() {
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("processing request")
+	subject := r.FormValue("subject")
+	body := r.FormValue("body")
+	to := getReceiver(r.URL)
+	go sendEmail(to, subject, body)
+	w.Write(STD_RESPONSE)
+}
+
+func getReceiver(u *url.URL) string {
+	//TODO: make sure that it is verified
+	log.Println(u.Path)
+	return "minhajuddin@mailinator.com"
+}
+
+func sendEmail(to, subject, body string) {
+	msg := fmt.Sprintf(`From: HTTP to Email <bot@cosmicvent.com>
+To: %s
+Subject: %s
+
+%s`, to, subject, body)
 	smtp.SendMail("localhost:25",
 		nil,
 		"HTTP to Email <bot@cosmicvent.com>",
-		[]string{"minhajuddin@mailinator.com"},
-		[]byte("This is awesome"))
+		[]string{to},
+		[]byte(msg))
 }
